9a822f99da2c84ed5eb26ab4b141d7aa0c177d92
Eric Prokop <eric.prokop@epnw.eu>
2021-06-28 00:05:57 +0200

    Update template files

--- NAME/lib/src/NAME_init.dart
+++ NAME/lib/src/NAME_init.dart
@@ -3,6 +3,12 @@
 import 'workarounds/web.dart' if (dart.library.io) 'workarounds/io.dart'
     as Workarounds;
 
+/// Registers the correct platform_interface for the current
+/// platform.
+///
+/// Until flutter is able to do this automatically, this has
+/// to be done manually. See the workarounds folder for more
+/// details!
 void init() {
   Workarounds.apply();
 }
--- NAME/lib/src/workarounds/web.dart
+++ NAME/lib/src/workarounds/web.dart
@@ -1,5 +1,5 @@
 // On web, there are currently no workarounds that need to be applied
 void apply() {
   // There still has to be an apply function, so that the
-  // conditional import in platform_interface.dart works!
+  // conditional import in NAME_init.dart works!
 }
