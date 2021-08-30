0e11dd9240d3cc354330446fae2003688673a330
Dominik <ds@indenware.de>
2021-08-31 00:07:53 +0200

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,34 +19,32 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-
-    final exp = Expression(
-      expression: "a / b",
-      variables: { "a": 4, "b": 2 }
-    );
-    print(exp.value); // 4/2=2
-
-    exp["a"] = 100;
-    exp["b"] = 300;
-    print(exp.value); // 100/300=0.333333...
-    
-    final exp2 = Expression(
-      expression: "clamp(-1.0,sin(2 * pi * x) + cos(x / 2 * pi),+1.0)",
-      variables: { "x": 0 }
-    );
-    for (double x = -5; x <= 5; x += 0.001)
-    {
-      exp2["x"] = x;
-      print("${exp2.value}");
-    }
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Expression plugin example'),
         ),
         body: Center(
-          child: Text('Running'),
+          child: OutlinedButton(
+            onPressed: () {
+
+              final exp2 = Expression(
+                expression: "clamp(-1.0,sin(2 * pi * x) + cos(x / 2 * pi),+1.0)",
+                variables: { "x": 0 }
+              );
+
+              final stopwatch = Stopwatch()..start();
+              for (double x = -5; x <= 5; x += 0.001)
+              {
+                exp2["x"] = x;
+                exp2.value;
+                // print("${exp2.value}");
+              }
+              print('Execution took ${stopwatch.elapsed.inMicroseconds} microseconds');
+
+            },
+            child: Text("Run"),
+          )
         ),
       ),
     );
